4116db3bc6601ed48a68c5a61aa5a9d4651a4e2a
levi <levielester@gmail.com>
2025-09-24 22:11:34 +0800

    Initial Commit

---
+++ MoodSprout/MoodSproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodSproutApp.swift
+//  MoodSprout
+//
+//  Created by Levi on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodSproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
